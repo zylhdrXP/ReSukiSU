d0e4c91c0a591f8f6fd60ec6cfa1b5fd4303e3d5
Alhaidar Latif <ziyalhaidar@gmail.com>
2026-09-08 01:00:44 +0700

    Update repository URL for KernelSU clone
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -41,7 +41,7 @@ setup_kernelsu() {
 	echo "[+] Setting up KernelSU..."
 	# Clone the repository and rename it to KernelSU
 	if [ ! -d "$GKI_ROOT/KernelSU" ]; then
-		git clone https://github.com/ReSukiSU/ReSukiSU KernelSU
+		git clone https://github.com/zylhdrXP/ReSukiSU KernelSU
 		echo "[+] Repository cloned."
 	fi
 	cd "$GKI_ROOT/KernelSU"
